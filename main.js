1656fef196033a74db09003368d585e0b5bbdd71
BrayanOFC <lourdesagueda93@gmail.com>
2025-06-12 13:27:12 -0700

    Actualizar main.js

--- main.js
+++ main.js
@@ -487,8 +487,4 @@ await purgeOldFiles()
 console.log(chalk.bold.cyanBright(`\n╭» 🟠 ARCHIVOS 🟠\n│→ ARCHIVOS RESIDUALES ELIMINADAS\n╰― ― ― ― ― ― ― ― ― ― ― ― ― ― ― ― ― ― ― 🗑️♻️`))}, 1000 * 60 * 10)
 
 _quickTest().then(() => conn.logger.info(chalk.bold(`👑  H E C H O\n`.trim()))).catch(console.error)
-
-async function joinChannels(conn) {
-for (const channelId of Object.values(global.ch)) {
-await await conn.newsletterFollow(channelId).catch(() => {})
 }}
