709d8b65ee779730952307d623469ed0477f1cc1
BrayanOFC <lourdesagueda93@gmail.com>
2025-07-20 18:52:39 -0700

    Actualizar owner-dsowner.js

--- plugins/owner-dsowner.js
+++ plugins/owner-dsowner.js
@@ -1,4 +1,4 @@
-import { readdirSync, unlinkSync, existsSync, promises as fs, rmSync } from 'fs'
+ import { readdirSync, unlinkSync, existsSync, promises as fs, rmSync } from 'fs'
 import path from 'path'
 
 var handler = async (m, { conn, usedPrefix }) => {
@@ -6,7 +6,7 @@ var handler = async (m, { conn, usedPrefix }) => {
 if (global.conn.user.jid !== conn.user.jid) {
 return conn.reply(m.chat, `${emoji} Utiliza este comando directamente en el número principal del Bot.`, m, rcanal);
 }
-await conn.reply(m.chat, `${emoji2} Iniciando proceso de eliminación de  los archivos de sesión, excepto el archivo creds.json...`, m, rcanal);
+await conn.reply(m.chat, `${emoji2} Iniciando proceso de eliminación de todos los archivos de sesión, excepto el archivo creds.json...`, m, rcanal);
 m.react(rwait)
 
 let sessionPath = `./${sessions}/`
@@ -14,7 +14,7 @@ let sessionPath = `./${sessions}/`
 try {
 
 if (!existsSync(sessionPath)) {
-return await conn.reply(m.chat, `${emoji} ʟᴀ ᴄᴀʀᴘᴇᴛᴀ ᴇsᴛᴀ ᴠᴀᴄɪᴀ✔️.`, m, rcanal);
+return await conn.reply(m.chat, `${emoji} La carpeta está vacía.`, m, rcanal);
 }
 let files = await fs.readdir(sessionPath)
 let filesDeleted = 0
@@ -28,12 +28,12 @@ if (filesDeleted === 0) {
 await conn.reply(m.chat, `${emoji2} La carpeta esta vacía.`, m, rcanal);
 } else {
 m.react(done)
-await conn.reply(m.chat, `${emoji} Se eliminaron ${filesDeleted} ᴀʀᴄʜɪᴠᴏs ᴅᴇ sᴇsɪᴏɴ, ᴇxᴄᴇᴘᴛᴏ ᴇʟ ᴀʀᴄʜɪᴠᴏ creds.json.`, m, rcanal);
+await conn.reply(m.chat, `${emoji} Se eliminaron ${filesDeleted} archivos de sesión, excepto el archivo creds.json.`, m, rcanal);
 conn.reply(m.chat, `${emoji} *¡Hola! ¿logras verme?*`, m, rcanal);
 
 }
 } catch (err) {
-console.error('ᴇʀʀᴏʀ ᴀʟ ʟᴇᴇʀ ʟᴀ ᴄᴀʀᴘᴇᴛᴀ ᴏ ʟᴏs ᴀʀᴄʜɪᴠᴏs ᴅᴇ sᴇsɪᴏɴ❌:', err);
+console.error('Error al leer la carpeta o los archivos de sesión:', err);
 await conn.reply(m.chat, `${msm} Ocurrió un fallo.`, m, rcanal);
 }
 
