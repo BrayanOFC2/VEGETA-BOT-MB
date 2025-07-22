691b23f0c9040efbbe161956b1914cdca3817126
BrayanOFC <lourdesagueda93@gmail.com>
2025-07-21 20:32:20 -0700

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,31 +1,34 @@
-function handler(m) {
-let name = conn.getName(`${numcreador}@s.whatsapp.net`)
-let ownerN = `${numcreador}`
-conn.sendContact(m.chat, [[`${ownerN}@s.whatsapp.net`, `${name}`]], m, {
- contextInfo: { 
- forwardingScore: 2023,
-isForwarded: false, 
- externalAdReply: {  
- title: packname, 
- body: dev, 
- sourceUrl: channel,
- thumbnail: banner,
- thumbnailUrl: banner, 
- mediaType: 1,
- showAdAttribution: true, 
- renderLargerThumbnail: true 
- }
-   }
-     },
-       {
-         quoted: m
-           }
-             );
+import PhoneNumber from 'awesome-phonenumber';
 
+async function handler(m, { conn }) { 
+    let numcreadora = '526641784469'; 
+    let ownerJid = numcreador + '@s.whatsapp.net';
+
+
+    let name = await conn.getName(ownerJid) || 'BrayanOFC'; 
+    let about = (await conn.fetchStatus(ownerJid).catch(() => {}))?.status || 'Sin descripción';
+
+
+    let vcard = `
+BEGIN:VCARD
+VERSION:3.0
+N:;${name};;;
+FN:${name}
+TEL;waid=${numcreador}:${new PhoneNumber('+' + numcreador).getNumber('international')}
+NOTE:${about}
+END:VCARD`.trim();
+
+
+    await conn.sendMessage(m.chat, { 
+        contacts: { 
+            displayName: name, 
+            contacts: [{ vcard }]
+        } 
+    }, { quoted: m });
 }
 
-handler.help = ['owner']
-handler.tags = ['main']
-handler.command = ['owner', 'creator', 'creador', 'dueño'] 
+handler.help = ['owner']; 
+handler.tags = ['main']; 
+handler.command = ['owner', 'creator', 'creador', 'dueño'];
 
-export default handler
+export default handler;
