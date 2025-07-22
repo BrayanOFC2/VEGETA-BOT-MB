5dafc1111b9c00f3a88882cfbd1fbbc155a045af
Deylin <deylinbo91@gmail.com>
2025-07-21 21:35:04 -0600

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,34 +1,46 @@
 import PhoneNumber from 'awesome-phonenumber';
 
-async function handler(m, { conn }) { 
-    let numcreadora = '5216641784469'; 
-    let ownerJid = numcreador + '@s.whatsapp.net';
+async function handler(m, { conn }) {
+  m.react('👑');
+  const numCreador = '5216633900512';
+  const ownerJid = numCreador + '@s.whatsapp.net';
+
+  const name = await conn.getName(ownerJid) || 'BrayanOFC';
+  const about = (await conn.fetchStatus(ownerJid).catch(() => {}))?.status || 'estoy disponible para responder a tus preguntas';
+  const empresa = 'BrayanOFC - Servicios Tecnológicos';
 
 
-    let name = await conn.getName(ownerJid) || 'BrayanOFC'; 
-    let about = (await conn.fetchStatus(ownerJid).catch(() => {}))?.status || 'Sin descripción';
-
-
-    let vcard = `
+  const vcard = `
 BEGIN:VCARD
 VERSION:3.0
 N:;${name};;;
 FN:${name}
-TEL;waid=${numcreador}:${new PhoneNumber('+' + numcreador).getNumber('international')}
+ORG:${empresa};
+TITLE:CEO & Fundador
+TEL;waid=${numCreador}:${new PhoneNumber('+' + numCreador).getNumber('international')}
+EMAIL:correo@empresa.com
+URL:https://www.tuempresa.com
 NOTE:${about}
-END:VCARD`.trim();
+ADR:;;Dirección de tu empresa;;;;
+X-ABADR:ES
+X-ABLabel:Dirección Web
+X-ABLabel:Correo Electrónico
+X-ABLabel:Teléfono de contacto
+X-WA-BIZ-NAME:${name}
+X-WA-BIZ-DESCRIPTION:${about}
+END:VCARD
+  `.trim();
 
 
-    await conn.sendMessage(m.chat, { 
-        contacts: { 
-            displayName: name, 
-            contacts: [{ vcard }]
-        } 
-    }, { quoted: m });
+  await conn.sendMessage(
+    m.chat,
+    { contacts: { displayName: name, contacts: [{ vcard }] } },
+    { quoted: fkontak }
+  );
 }
 
-handler.help = ['owner']; 
-handler.tags = ['main']; 
+handler.help = ['owner'];
+handler.tags = ['main'];
 handler.command = ['owner', 'creator', 'creador', 'dueño'];
 
 export default handler;
