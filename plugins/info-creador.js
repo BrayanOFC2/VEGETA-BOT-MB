e0c154ccaa19d4014fbaddf1be103bfed20c2a02
BrayanOFC <lourdesagueda93@gmail.com>
2025-07-21 23:43:25 -0700

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,47 +1,14 @@
-import PhoneNumber from 'awesome-phonenumber';
-
 async function handler(m, { conn }) {
   m.react('👑');
 
-  const numCreador = '5216641784469';
-  const name = 'BʀᴀʏᴀɴOFC👻';
-  const about = 'estoy disponible para responder a tus preguntas';
+  const name = 'BrᴀʏᴀɴOFC👻';
   const empresa = 'BrayanOFC - Servicios Tecnológicos';
-  const link = 'https://wa.me/' + numCreador;
+  const link = 'https://wa.me/5216641784469';
 
   await conn.sendMessage(m.chat, {
     image: { url: 'https://qu.ax/gSWtg.jpg' },
-    caption: `👑 *${name}*\n👻 - CEO & Fundador de *${empresa}*\n\n📲 ${link}`,
+    caption: `👑 *${name}*\n👻 - CEO & Fundador de\n*${empresa}*\n\n📲 ➡️ ${link}`,
   }, { quoted: m });
-
-  const vcard = `
-BEGIN:VCARD
-VERSION:3.0
-N:;BrayanOFC;;;
-FN:BrayanOFC
-ORG:${empresa};
-TITLE:CEO & Fundador
-TEL;waid=${numCreador}:${new PhoneNumber('+' + numCreador).getNumber('international')}
-EMAIL:correo@empresa.com
-URL:https://www.tuempresa.com
-NOTE:${about}
-ADR:;;Dirección de tu empresa;;;;
-X-ABADR:ES
-X-WA-BIZ-NAME:BrayanOFC
-X-WA-BIZ-DESCRIPTION:${about}
-END:VCARD
-`.trim();
-
-  await conn.sendMessage(
-    m.chat,
-    {
-      contacts: {
-        displayName: 'BrayanOFC',
-        contacts: [{ vcard }]
-      }
-    },
-    { quoted: m }
-  );
 }
 
 handler.help = ['owner'];
