db5099ec1c407fc9fec2b7f2c953f9694979a343
BrayanOFC <lourdesagueda93@gmail.com>
2025-07-28 01:28:18 -0700

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -96,7 +96,7 @@ ${commandsForTag.map(menu => menu.help.map(help =>
     await m.react('👑')
 
     await conn.sendMessage(m.chat, {
-      video: { url: 'https://files.catbox.moe/0d5eiu.mp4' },
+      video: { url: 'https://qu.ax/BYKaE.mp4' },
       caption: menuText,
       gifPlayback: true,
       contextInfo: {
