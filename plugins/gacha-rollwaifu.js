6343365360b712d046772c78b7c33511bd7cab3e
BrayanOFC <lourdesagueda93@gmail.com>
2025-07-30 22:24:54 -0700

    Actualizar gacha-rollwaifu.js

--- plugins/gacha-rollwaifu.js
+++ plugins/gacha-rollwaifu.js
@@ -44,10 +44,10 @@ let handler = async (m, { conn }) => {
     const now = Date.now();
 
     if (cooldowns[userId] && now < cooldowns[userId]) {
-        const remainingTime = Math.ceil((cooldowns[userId] - now) / 10);
-        //const minutes = Math.floor(remainingTime / 60);
+        const remainingTime = Math.ceil((cooldowns[userId] - now) / 1000);
+        const minutes = Math.floor(remainingTime / 60);
         const seconds = remainingTime % 10;
-        return await conn.reply(m.chat, `《✧》Debes esperar ${minutos} y ${seconds} segundos* para usar *#rw* de nuevo.`, m);
+        return await conn.reply(m.chat, `《✧》Debes esperar ${minutos} minutos y ${seconds} segundos* para usar *#rw* de nuevo.`, m);
     }
 
     try {
