7bbe73277bd7f6a412181f016c415221372d6135
BrayanOFC <lourdesagueda93@gmail.com>
2025-07-31 23:31:10 -0700

    Actualizar info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,8 +1,14 @@
-import fetch from 'node-fetch'
+import fetch from 'node-fetch';
+import { readFile } from 'fs/promises';
 
-let handler  = async (m, { conn, usedPrefix, command }) => {
+let handler = async (m, { conn, usedPrefix, command }) => {
+  const data = JSON.parse(await readFile('./src/database/db.json', 'utf-8'));
+  const imagenes = data.vegeta?.imagenes;
+  const miniurl = imagenes?.length
+    ? imagenes[Math.floor(Math.random() * imagenes.length)]
+    : 'https://i.imgur.com/XTzfa1T.jpg';
 
-let grupos = `*Hola!, te invito a unirte a los grupos oficiales del Bot para convivir con la comunidad.....*
+  let grupos = `*Hola!, te invito a unirte a los grupos oficiales del Bot para convivir con la comunidad.....*
 
 - ${namegrupo}
 *❀* ${gp1}
@@ -20,14 +26,15 @@ let grupos = `*Hola!, te invito a unirte a los grupos oficiales del Bot para con
 - ✧┊┋◟𝐕𝐞𝐠𝐞𝐭𝐚 𝐁𝐨𝐭 𝐓𝐞𝐬𝐭◞┊┋✧
 *❀* ${channel2}
 
-> ${dev}`
+> ${dev}`;
 
-await conn.sendFile(m.chat, miniurl, "vegeta.jpg", grupos, m, null, rcanal)
+  await conn.sendFile(m.chat, miniurl, "vegeta.jpg", grupos, m, null, rcanal);
 
-await m.react(emojis)
+  await m.react(emojis);
+};
 
-}
-handler.help = ['grupos']
-handler.tags = ['info']
-handler.command = ['grupos', 'links', 'groups']
-export default handler
+handler.help = ['grupos'];
+handler.tags = ['info'];
+handler.command = ['grupos', 'links', 'groups'];
+
+export default handler;
