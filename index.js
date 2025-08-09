4f88fbad07d4e0730f801c4fb50585e41d7ffced
BrayanOFC <lourdesagueda93@gmail.com>
2025-08-08 18:02:28 -0700

    Actualizar index.js

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ serialize()
 
 global.__filename = function filename(pathURL = import.meta.url, rmPrefix = platform !== 'win32') {
 return rmPrefix ? /file:////.test(pathURL) ? fileURLToPath(pathURL) : pathURL : pathToFileURL(pathURL).toString();
-}
+
 global.__dirname = function dirname(pathURL) {
 return path.dirname(global.__filename(pathURL, true))
 }
