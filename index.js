4cd0a2343138ad19d6ba85ca1b39ff3133b5dd95
BrayanOFC <lourdesagueda93@gmail.com>
2025-08-11 21:54:49 -0700

    Actualizar index.js

--- index.js
+++ index.js
@@ -194,6 +194,7 @@ codeBot = codeBot?.match(/.{1,4}/g)?.join("-") || codeBot
 console.log(chalk.bold.white(chalk.bgMagenta(`☁️ CÓDIGO DE VINCULACIÓN SAIYAJIN 👑 `)), chalk.bold.white(chalk.white(codeBot)))
 }, 3000)
 }}}
+}
 
 conn.isInit = false;
 conn.well = false;
