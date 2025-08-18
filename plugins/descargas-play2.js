8a8c79ba6a37975944b54f620b11d9764d39c929
BrayanOFC <lourdesagueda93@gmail.com>
2025-08-18 16:42:19 -0700

    Actualizar descargas-play2.js

--- plugins/descargas-play2.js
+++ plugins/descargas-play2.js
@@ -1,4 +1,4 @@
-import fetch from "node-fetch";
+/*import fetch from "node-fetch";
 import yts from 'yt-search';
 import axios from "axios";
 
@@ -146,4 +146,4 @@ function formatViews(views) {
   } else {
     return views.toString();
   }
-}
+}*/
