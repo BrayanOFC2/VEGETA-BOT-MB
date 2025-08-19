9bddf97231f089b72804e3e66a16f78a565a1c3c
Deylin <deylinbo91@gmail.com>
2025-08-19 17:29:12 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -1,19 +1,26 @@
 const handler = async (m, { conn }) => {
     
-    const rcanal = {
-  contextInfo: {
-    externalAdReply: {
-      showAdAttribution: true, 
-      title: botname,
-      body: dev,
-      mediaType: 2,         
-      thumbnailUrl: global.icono,
-      sourceUrl: redes
-    }
-  }
-}
+ const res = await fetch('https://files.catbox.moe/u5ohu2.png');
+const img = Buffer.from(await res.arrayBuffer());
 
-    await conn.reply(m.chat, "Prueba", m, rcanal);
+const fkontak = {
+    key: { fromMe: false, participant: "0@s.whatsapp.net" },
+    message: {
+        productMessage: {
+            product: {
+                productImage: { jpegThumbnail: img },
+                title: botname,
+                description: "venta bot",
+                currencyCode: "USD",
+                priceAmount1000: "5000", 
+                retailerId: "BOT"
+            },
+            businessOwnerJid: "0@s.whatsapp.net"
+        }
+    }
+};
+
+    await conn.reply(m.chat, "Prueba", fkontak, rcanal);
 };
 
 handler.command = ['1'];
