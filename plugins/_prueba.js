8e74a4ee02fac5c37e73958163746f6231f05770
Deylin <deylinbo91@gmail.com>
2025-08-19 15:50:26 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -1,14 +1,10 @@
 const handler = async (m, { conn }) => {
-const res = await fetch("https://i.postimg.cc/d3Q1g80b/IMG-20250803-WA0147.jpg");
-const thumb = Buffer.from(await res.arrayBuffer());
-
 const rcanal = {
   contextInfo: {
     externalAdReply: {
       title: botname,
       body: dev,
-      mediaType: 2,
-      jpegThumbnail: thumb
+      thumbnailUrl: icono
     }
   }
 }
