2e3adf2bc4b6a0caa40231e2d54e81894ef962a4
BrayanOFC <lourdesagueda93@gmail.com>
2025-08-18 20:21:13 -0700

    Actualizar index.js

--- index.js
+++ index.js
@@ -65,9 +65,9 @@ console.log(chalk.bold.magentaBright('╚═════════════
 protoType()
 serialize()
 
-global.__filename = function filename(pathURL = import.meta.url, rmPrefix = platform !== 'win32') }
+global.__filename = function filename(pathURL = import.meta.url, rmPrefix = platform !== 'win32') {
 return rmPrefix ? /file:////.test(pathURL) ? fileURLToPath(pathURL) : pathURL : pathToFileURL(pathURL).toString();
-}; global.__dirname = function dirname(pathURL) {
+}; global.__dirname = function dirname(pathURL) }
 return path.dirname(global.__filename(pathURL, true))
 }; global.__require = function require(dir = import.meta.url) {
 return createRequire(dir)
