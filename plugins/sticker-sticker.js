fd1bf876be5615bcb632f3e10d39c31b3937dce9
Wichi <geraldorincon52@gmail.com>
2025-08-19 19:40:21 -0400

    Actualizar sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -13,7 +13,7 @@ if (/webp|image|video/g.test(mime)) {
 if (/video/g.test(mime)) if ((q.msg || q).seconds > 15) return m.reply(`👑 ¡El video no puede durar más de 15 segundos!...`)
 let img = await q.download?.()
 
-if (!img) return conn.reply(m.chat, `🐉 Por favor, envia una imagen o video para hacer un sticker.`, m, rcanal)
+if (!img) return conn.reply(m.chat, `🐉 Por favor, envia una imagen o video para hacer un sticker.`, m,fake)
 
 let out
 try {
@@ -40,7 +40,7 @@ if (!stiker) stiker = e
 } finally {
 if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: packname, body: dev, mediaType: 2, sourceUrl: redes, thumbnail: icons}}}, { quoted: m })
 
-else return conn.reply(m.chat, '🐉 Por favor, envia una imagen o video para hacer un sticker.', m, rcanal)
+else return conn.reply(m.chat, '🐉 Por favor, envia una imagen o video para hacer un sticker.', m,fake)
 
 
 }}
