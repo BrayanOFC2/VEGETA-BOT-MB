8f329327e5974d7955acbe2607e8b9d054f1c06f
BrayanOFC <lourdesagueda93@gmail.com>
2025-08-20 17:30:57 -0700

    Actualizar grupo-setwelcome.js

--- plugins/grupo-setwelcome.js
+++ plugins/grupo-setwelcome.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, text, isRowner }) => {
-  if (!text) return m.reply('🍬 Por favor, proporciona una bienvenida para el bot.\n> Ejemplo: #setwelcome Hola user');
+  if (!text) return m.reply('🐉Por favor, proporciona una bienvenida para el bot.\n> Ejemplo: #setwelcome Hola user');
 
   global.welcom1 = text.trim();
   
-  m.reply(`🍬 La bienvenida del bot ha sido cambiado a: ${global.welcom1}`);
+  m.reply(`☁️ La bienvenida del bot ha sido cambiado a: ${global.welcom1}`);
 };
 
 handler.help = ['setwelcome'];
