662d67eb55e89a3d5f20d41920b005a869a5f38b
BrayanOFC <lourdesagueda93@gmail.com>
2025-08-20 17:57:30 -0700

    Actualizar nsfw-footjob.js

--- plugins/nsfw-footjob.js
+++ plugins/nsfw-footjob.js
@@ -1,11 +1,11 @@
-//Codígo creado por Brayan wa.me/526641804242
+//Codígo creado por BrayanOFC wa.me/526641804242
 
 import fs from 'fs';
 import path from 'path';
 
 let handler = async (m, { conn, usedPrefix }) => {
 if (!db.data.chats[m.chat].nsfw && m.isGroup) {
-    return m.reply('🍬 El contenido *NSFW* está desactivado en este grupo.\n> Un administrador puede activarlo con el comando » *#nsfw*');
+    return m.reply('🐉 El contenido *NSFW* está desactivado en este grupo.\n> Un administrador puede activarlo con el comando » *#nsfw*');
     }
 
     let who;
