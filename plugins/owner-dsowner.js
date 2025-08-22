f545463911f59dc2247f4e239f12880544d22386
BrayanOFC <lourdesagueda93@gmail.com>
2025-08-22 00:08:47 -0700

    Actualizar owner-dsowner.js

--- plugins/owner-dsowner.js
+++ plugins/owner-dsowner.js
@@ -1,12 +1,14 @@
+/* Codigo hecho por @Fabri115 y mejorado por BrunoSobrino */
+
 import { readdirSync, unlinkSync, existsSync, promises as fs, rmSync } from 'fs'
 import path from 'path'
 
 var handler = async (m, { conn, usedPrefix }) => {
 
 if (global.conn.user.jid !== conn.user.jid) {
-return conn.reply(m.chat, `${emojis} Utiliza este comando directamente en el número principal del Bot.`, m, rcanal);
+return conn.reply(m.chat, `${emoji} Utiliza este comando directamente en el número principal del Bot.`, m)
 }
-//await conn.reply(m.chat, `${emoji2} Iniciando proceso de eliminación de todos los archivos de sesión, excepto el archivo creds.json...`, m, rcanal);
+await conn.reply(m.chat, `${emoji2} Iniciando proceso de eliminación de todos los archivos de sesión, excepto el archivo creds.json...`, m)
 m.react(rwait)
 
 let sessionPath = `./${sessions}/`
@@ -14,7 +16,7 @@ let sessionPath = `./${sessions}/`
 try {
 
 if (!existsSync(sessionPath)) {
-return await conn.reply(m.chat, `${emojis} La carpeta está vacía.`, m, rcanal);
+return await conn.reply(m.chat, `${emoji3} La carpeta está vacía.`, m)
 }
 let files = await fs.readdir(sessionPath)
 let filesDeleted = 0
@@ -25,16 +27,16 @@ filesDeleted++;
 }
 }
 if (filesDeleted === 0) {
-await conn.reply(m.chat, `${emoji2} La carpeta esta vacía.`, m, fake);
+await conn.reply(m.chat, `${emoji2} La carpeta esta vacía.`, m)
 } else {
 m.react(done)
-await conn.reply(m.chat, `${emojis} Se eliminaron ${filesDeleted} archivos de sesión, excepto el archivo creds.json.`, m, rcanal);
-conn.reply(m.chat, `${emojis} *¡Hola! ¿logras verme?*`, m, rcanal);
+await conn.reply(m.chat, `${emoji} Se eliminaron ${filesDeleted} archivos de sesión, excepto el archivo creds.json.`, m)
+conn.reply(m.chat, `${emoji} *¡Hola! ¿logras verme?*`, m)
 
 }
 } catch (err) {
 console.error('Error al leer la carpeta o los archivos de sesión:', err);
-await conn.reply(m.chat, `${msm} Ocurrió un fallo.`, m, rcanal);
+await conn.reply(m.chat, `${msm} Ocurrió un fallo.`, m)
 }
 
 }
