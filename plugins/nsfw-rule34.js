cd2bdbb27504f18847b7ff08055f68409d057fe5
Deylin <deylinbo91@gmail.com>
2025-08-22 11:11:28 -0600

    Actualizar nsfw-rule34.js

--- plugins/nsfw-rule34.js
+++ plugins/nsfw-rule34.js
@@ -1,10 +1,10 @@
 import fetch from 'node-fetch';
 const handler = async (m, { conn, args, usedPrefix }) => {
-    if (!args[0]) {
-if (!db.data.chats[m.chat].nsfw && m.isGroup) {
-    return m.reply('🐉 El contenido *NSFW* está desactivado en este grupo.\n> Un administrador puede activarlo con el comando » *#nsfw*');
+    if (!db.data.chats[m.chat].nsfw && m.isGroup) {
+    return m.reply(`${emoji} El contenido *NSFW* está desactivado en este grupo.\n> Un administrador puede activarlo con el comando » *#nsfw on*`);
     }
-      await conn.reply(m.chat, '☁️ Por favor, ingresa un tag para realizar la búsqueda.', m, fake);
+    if (!args[0]) {
+      await conn.reply(m.chat, `${emoji} Por favor, ingresa un tag para realizar la búsqueda.`, m);
         return;
     }
     const tag = args[0];
@@ -13,19 +13,20 @@ if (!db.data.chats[m.chat].nsfw && m.isGroup) {
         const response = await fetch(url);
         const data = await response.json();
         if (!data || data.length === 0) {
-            await conn.reply(m.chat, `📍 No hubo resultados para *${tag}*`, m, fake);
+            await conn.reply(m.chat, `${emoji2} No hubo resultados para *${tag}*`, m);
             return;
         }
         const randomIndex = Math.floor(Math.random() * data.length);
         const randomImage = data[randomIndex];
         const imageUrl = randomImage.file_url;
-        await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: `👑 *Resultados para* » ${tag}`, mentions: [m.sender] });
+        await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: `${emoji} Resultados para » *${tag}*`, mentions: [m.sender] });
     } catch (error) {
         console.error(error);
-        await m.reply('⚠️ Ocurrió un error inesperado.');
+        await m.reply(`${emoji} Ocurrió un error.`);
     }
 };
 handler.help = ['r34 <tag>', 'rule34 <tag>'];
 handler.command = ['r34', 'rule34'];
 handler.tags = ['nsfw'];
+
 export default handler;
