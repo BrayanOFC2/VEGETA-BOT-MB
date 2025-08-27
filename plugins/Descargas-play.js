1b7ef0563de93910c3a41cbeeee1925f260fd5e0
BrayanOFC <lourdesagueda93@gmail.com>
2025-08-27 16:47:22 -0700

    Actualizar Descargas-play.js

--- plugins/Descargas-play.js
+++ plugins/Descargas-play.js
@@ -1,59 +1,3 @@
-/*import fetch from 'node-fetch';
-
-let handler = async (m, { conn, usedPrefix, command, text }) => {
-  if (!text) return m.reply(`🐉 Ingresa un texto para buscar en YouTube.\n> *Ejemplo:* ${usedPrefix + command} Shakira`);
-
-  try {
-    const searchApi = `https://delirius-apiofc.vercel.app/search/ytsearch?q=${text}`;
-    const searchResponse = await fetch(searchApi);
-    const searchData = await searchResponse.json();
-
-    if (!searchData?.data || searchData.data.length === 0) {
-      return m.reply(`⚠️ No se encontraron resultados para "${text}".`);
-    }
-
-    const video = searchData.data[0]; // Tomar el primer resultado
-    const videoDetails = `
-🎵 *Título:* ${video.title}
-📺 *Canal:* ${video.author.name}
-⏱️ *Duración:* ${video.duration}
-👀 *Vistas:* ${video.views}
-📅 *Publicado:* ${video.publishedAt}
-🌐 *Enlace:* ${video.url}
-`;
-
-    await conn.sendMessage(m.chat, {
-      image: { url: video.image },
-      caption: videoDetails.trim()
-    }, { quoted: m });
-
-    const downloadApi = `https://api.vreden.my.id/api/ytmp3?url=${video.url}`;
-    const downloadResponse = await fetch(downloadApi);
-    const downloadData = await downloadResponse.json();
-
-    if (!downloadData?.result?.download?.url) {
-      return m.reply("☁️ No se pudo obtener el audio del video.");
-    }
-    await conn.sendMessage(m.chat, {
-      audio: { url: downloadData.result.download.url },
-      mimetype: 'audio/mpeg',
-      fileName: `${video.title}.mp3`
-    }, { quoted: m });
-
-    await m.react("✅");
-  } catch (error) {
-    console.error(error);
-    m.reply(`❌ Error al procesar la solicitud:\n${error.message}`);
-  }
-};
-
-handler.command = ['play', 'playaudio'];
-handler.help = ['play <texto>', 'playaudio <texto>'];
-handler.tags = ['media'];
-
-export default handler;*/
-
-
 import yts from 'yt-search';
 import fetch from 'node-fetch';
 import { generateWAMessageFromContent, proto, prepareWAMessageMedia } from '@whiskeysockets/baileys';
@@ -87,8 +31,7 @@ const handler = async (m, { conn, args, usedPrefix, command }) => {
                         hydratedFooterText: '𝖯𑄜𝗐𝖾𝗋𝖾𝖽 𝖻𝗒 𝖲𝗁⍺𝖽ᦅ𝗐′𝗌 𝖢𝗅𝗎𝖻',
                         hydratedButtons: [
                             { quickReplyButton: { displayText: '🎵 Audio', id: `${usedPrefix}ytmp3 ${video.url}` } },
-                            { quickReplyButton: { displayText: '🎬 Video', id: `${usedPrefix}ytmp4 ${video.url}` } },
-                            { urlButton: { displayText: '🌐 Ver Video', url: video.url } }
+                            { quickReplyButton: { displayText: '🎬 Video', id: `${usedPrefix}ytmp4 ${video.url}` } }
                         ]
                     }
                 }
