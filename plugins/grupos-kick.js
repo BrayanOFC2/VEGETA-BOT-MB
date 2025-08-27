cbe5e5d6e2b45a3c574fde0f26fa4baa60ef3244
BrayanOFC <lourdesagueda93@gmail.com>
2025-08-27 15:56:27 -0700

    Actualizar grupos-kick.js

--- plugins/grupos-kick.js
+++ plugins/grupos-kick.js
@@ -24,7 +24,7 @@ var handler = async (m, { conn, participants, usedPrefix, command }) => {
 
     await conn.groupParticipantsUpdate(m.chat, [user], 'remove');
 
-//conn.reply('526641784469@s.whatsapp.net', `🔰 Un Admin Acabo De Eliminar Un Usuario En El Grupo:\n> ${groupMetadata.subject}.`, m, rcanal, );
+conn.reply('526641784469@s.whatsapp.net', `🔰 Un Admin Acabo De Eliminar Un Usuario En El Grupo:\n> ${groupMetadata.subject}.`, m, rcanal, );
 };
 
 handler.help = ['kick'];
