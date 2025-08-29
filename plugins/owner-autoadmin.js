58cf9d34b50845841072d66cdf688fa697b08ff6
BrayanOFC <lourdesagueda93@gmail.com>
2025-08-28 22:18:52 -0700

    Actualizar owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++ plugins/owner-autoadmin.js
@@ -3,7 +3,7 @@ const handler = async (m, {conn, isAdmin, groupMetadata }) => {
   try {
     await conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote');
   await m.react(done)
-   m.reply('👑 *Ya quedó patrón🥷.*');
+   m.reply('👑 *Ya quedó patrón.🥷*');
   } catch {
     m.reply('⚠️ Ocurrio un error.');
   }
